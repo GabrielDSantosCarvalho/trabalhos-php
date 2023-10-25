f0146be8b5c6823e0dc361dfa12564e4d182c1d4
Gabriel Carvalho <146982565+GabrielDSantosCarvalho@users.noreply.github.com>
2023-10-25 14:48:32 -0300

    Add files via upload

---
+++ tarefas de laços/tarefa laços.php
@@ -0,0 +1,16 @@
+ <?php
+for ($numero = 0; $numero <= 500; $numero += 2) {
+    echo (" " . $numero . " ");
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+for ($numero = 1; $numero <= 500; $numero += 2) {
+    echo (" " . $numero . " ");
+}
+?> 
+
+
